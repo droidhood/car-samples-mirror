9576397bcf2df28d40c6cd308217e68f84913b4e
Rafael Lima <therafael@google.com>
2021-07-27 23:52:05 +0000

    Added ability to set a custom branded background for permission request
    Relnote: Added ability to set a custom branded background for permission request
    Test: Manual
    Bug: 188093439
    Change-Id: I74b767b7d5f04928c0210a8685e486330e8df870

--- car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/misc/NotificationDemoScreen.java
+++ car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/misc/NotificationDemoScreen.java
@@ -108,8 +108,6 @@ public final class NotificationDemoScreen extends Screen implements DefaultLifec
 
     @Override
     public void onDestroy(@NonNull LifecycleOwner owner) {
-        mHandler.removeMessages(MSG_SEND_NOTIFICATION);
-        CarNotificationManager.from(getCarContext()).cancelAll();
         unregisterBroadcastReceiver();
     }
 
