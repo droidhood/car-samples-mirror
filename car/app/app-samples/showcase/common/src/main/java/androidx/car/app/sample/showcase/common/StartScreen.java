a0f62ccfb37345dfb1f617ed9732205eeebaa5fb
Treehugger Robot <treehugger-gerrit@google.com>
2022-05-11 21:42:06 +0000

    Merge "Fix favorite header button behavior." into androidx-main

--- car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/StartScreen.java
+++ car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/StartScreen.java
@@ -121,9 +121,7 @@ public final class StartScreen extends Screen {
                         .build());
         return new ListTemplate.Builder()
                 .setSingleList(listBuilder.build())
-                .setTitle(getCarContext().getString(R.string.showcase_demos_title) + " ("
-                        + getCarContext().getString(R.string.cal_api_level_prefix,
-                        getCarContext().getCarAppApiLevel()) + ")")
+                .setTitle(getCarContext().getString(R.string.showcase_demos_title))
                 .setHeaderAction(Action.APP_ICON)
                 .build();
     }
