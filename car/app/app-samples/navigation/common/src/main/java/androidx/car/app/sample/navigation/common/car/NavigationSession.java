588e7773a33d36f692acf99b450decf98683ad9f
Aurimas Liutikas <aurimas@google.com>
2022-08-08 17:17:50 +0000

    Revert "Upgrade to AGP 7.4.0-alpha09"
    Revert submission 2175882-agp-7.4.0-alpha09
    
    Reason for revert: Causing build flakiness
    Reverted Changes:
    I2d71ac4d4:Add prebuilts for AGP 7.4.0-alpha09
    I66662f921:Upgrade to AGP 7.4.0-alpha09
    
    Change-Id: Ic782e63042b6bac4e30d73f3311d6c030b50f098

--- car/app/app-samples/navigation/common/src/main/java/androidx/car/app/sample/navigation/common/car/NavigationSession.java
+++ car/app/app-samples/navigation/common/src/main/java/androidx/car/app/sample/navigation/common/car/NavigationSession.java
@@ -37,7 +37,6 @@ import androidx.car.app.CarToast;
 import androidx.car.app.Screen;
 import androidx.car.app.ScreenManager;
 import androidx.car.app.Session;
-import androidx.car.app.SessionInfo;
 import androidx.car.app.model.Action;
 import androidx.car.app.model.CarIcon;
 import androidx.car.app.model.Distance;
@@ -175,11 +174,9 @@ class NavigationSession extends Session implements NavigationScreen.Listener {
                 }
             };
 
-    NavigationSession(@NonNull SessionInfo sessionInfo) {
-        if (sessionInfo.getDisplayType() == SessionInfo.DISPLAY_TYPE_MAIN) {
-            Lifecycle lifecycle = getLifecycle();
-            lifecycle.addObserver(mLifeCycleObserver);
-        }
+    NavigationSession() {
+        Lifecycle lifecycle = getLifecycle();
+        lifecycle.addObserver(mLifeCycleObserver);
     }
 
     @Override
