b87305ca95ff315f075f6fd5cf750f60f8b09fee
Kodlee Yin <kodlee@google.com>
2025-01-31 22:09:00 -0800

    Add simple SectionedItemTemplateDemoScreen
    Bug: 393696010
    Fixes: 393696010
    Test: No testable logic
    Relnote: N/A
    Change-Id: I170c14384764bc64ca171250606d9e9f51ecb269

--- car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/screens/templatelayouts/SectionedItemTemplateDemoScreen.kt
+++ car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/screens/templatelayouts/SectionedItemTemplateDemoScreen.kt
@@ -13,68 +13,101 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package androidx.car.app.sample.showcase.common.screens.templatelayouts
 
+import android.util.Log
 import androidx.annotation.OptIn
 import androidx.car.app.CarContext
+import androidx.car.app.CarToast
 import androidx.car.app.Screen
 import androidx.car.app.annotations.ExperimentalCarApi
-import androidx.car.app.annotations.RequiresCarApi
 import androidx.car.app.model.Action
 import androidx.car.app.model.Header
 import androidx.car.app.model.Row
 import androidx.car.app.model.RowSection
 import androidx.car.app.model.SectionedItemTemplate
 import androidx.car.app.model.Template
+import androidx.car.app.model.Toggle
 import androidx.car.app.sample.showcase.common.R
-import androidx.car.app.sample.showcase.common.screens.templatelayouts.sectioneditemtemplates.AlphaJumpDemoScreen
-import androidx.car.app.sample.showcase.common.screens.templatelayouts.sectioneditemtemplates.EndImageAndActionsDemo
-import androidx.car.app.sample.showcase.common.screens.templatelayouts.sectioneditemtemplates.SimpleListDemoScreen
 
-@RequiresCarApi(8)
+/** Demonstrates the usage of the [SectionedItemTemplate]. */
 @OptIn(ExperimentalCarApi::class)
-/** A screen demonstrating lists with sectioned item list using the SectionedItemTemplate. */
 class SectionedItemTemplateDemoScreen(carContext: CarContext) : Screen(carContext) {
     override fun onGetTemplate(): Template {
-        return SectionedItemTemplate.Builder()
-            .addSection(
-                RowSection.Builder()
-                    .setTitle("SectionedItemTemplate Demo Screen")
-                    .addItem(
-                        buildRowForTemplate(
-                            SimpleListDemoScreen(carContext),
-                            R.string.sectioned_item_list_demo_title,
-                        )
+        val builder =
+            SectionedItemTemplate.Builder()
+                .setHeader(
+                    Header.Builder()
+                        .setTitle(carContext.getString(R.string.sectioned_item_template_demo_title))
+                        .setStartHeaderAction(Action.BACK)
+                        .build()
+                )
+
+        builder.addSection(
+            createRowSectionBuilder(
+                    sectionTitle =
+                        carContext.getString(
+                            R.string.sectioned_item_template_radio_button_section_title
+                        ),
+                    numberOfRows = 5
+                ) { rowBuilder, index ->
+                    rowBuilder.setOnClickListener { showToast("Active radio button index: $index") }
+                }
+                .setAsSelectionGroup(1)
+                .build()
+        )
+        builder.addSection(
+            createRowSectionBuilder(
+                    sectionTitle =
+                        carContext.getString(R.string.sectioned_item_template_toggle_section_title),
+                    numberOfRows = 5
+                ) { rowBuilder, index ->
+                    rowBuilder.setToggle(
+                        Toggle.Builder { isToggled -> showToast("$index: $isToggled") }.build()
                     )
-                    .addItem(
-                        buildRowForTemplate(
-                            AlphaJumpDemoScreen(carContext),
-                            R.string.sectioned_item_alpha_jump_demo_title,
-                        )
-                    )
-                    .addItem(
-                        buildRowForTemplate(
-                            EndImageAndActionsDemo(carContext),
-                            R.string.end_image_and_action_demo_title,
-                        )
-                    )
-                    .build()
-            )
-            .setHeader(
-                Header.Builder()
-                    .setTitle(carContext.getString(R.string.sectioned_item_template_demo_title))
-                    .setStartHeaderAction(Action.BACK)
-                    .build()
-            )
-            .build()
+                }
+                .build()
+        )
+        builder.addSection(
+            createRowSectionBuilder(
+                    sectionTitle =
+                        carContext.getString(
+                            R.string.sectioned_item_template_lots_of_rows_section_title
+                        ),
+                    numberOfRows = 150
+                )
+                .build()
+        )
+
+        return builder.build()
     }
 
-    private fun buildRowForTemplate(screen: Screen, title: Int): Row {
-        return Row.Builder()
-            .setTitle(carContext.getString(title))
-            .setOnClickListener { screenManager.push(screen) }
-            .setBrowsable(true)
-            .build()
+    /**
+     * Creates [RowSection.Builder] prepopulated with [numberOfRows] that were created with a
+     * default title and subtitle and modified by [rowBuilderAugment].
+     *
+     * @param[sectionTitle] a string to set as the section's title
+     * @param[numberOfRows] the number of rows to generate in this section
+     * @param[rowBuilderAugment] augments that are applied to each row as its being built
+     */
+    private fun createRowSectionBuilder(
+        sectionTitle: String,
+        numberOfRows: Int,
+        rowBuilderAugment: ((builder: Row.Builder, index: Int) -> Unit)? = null
+    ): RowSection.Builder {
+        val builder = RowSection.Builder().setTitle(sectionTitle)
+
+        for (i in 0 until numberOfRows) {
+            val rowBuilder = Row.Builder().setTitle("Row $i").addText("This is subtext")
+            rowBuilderAugment?.invoke(rowBuilder, i)
+            builder.addItem(rowBuilder.build())
+        }
+        return builder
+    }
+
+    private fun showToast(text: String) {
+        // Toasts are sometimes throttled, so we log as well to guarantee an output somewhere
+        Log.i(SectionedItemTemplateDemoScreen::class.simpleName, text)
+        CarToast.makeText(carContext, text, CarToast.LENGTH_SHORT).show()
     }
 }
